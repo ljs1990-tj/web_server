d3e3d420db20d3f94e640cd0771287d839cda81a
ljs1990-tj <ljs1990.tj@gmail.com>
2025-04-24 11:44:53 +0900

    2025.04.24 jwt 중간 커밋

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const db = require('./db');
 const productRouter = require('./routes/product')
 const userRouter = require('./routes/user')
+const loginRouter = require('./routes/login');
+const path = require('path');
 const cors = require('cors') 
 var session = require('express-session')
 
 const app = express()
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors({
     origin : "http://localhost:5502",
     credentials : true
@@ -25,6 +28,7 @@ app.use(session({
 
 app.use("/product", productRouter);
 app.use("/user", userRouter);
+app.use("/login", loginRouter);
 
 app.listen(3000, ()=>{
     console.log("서버 실행 중!"); 
